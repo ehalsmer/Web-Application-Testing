e1b252a3b309d14c3821a54ff0545d4c444f90e7
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-08 14:57:22 -0600

    Wrote tests for clicks to fire events on buttons, and wrote functions to handle strike and ball counting logic

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,19 +4,57 @@
 - changes recorded on this component should update the information shown by the `Display` component.
 */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 // four buttons: strike, ball, foul, hit.
 // Counter functions for balls and strikes.
 // Current count of each sent to Display.js as props.
 
 function Dashboard() {
+  const [balls, setBalls] = useState(0);
+  const [strikes, setStrikes] = useState(0);
+
+
+  /* 
+  - balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls.
+- balls and strikes reset to 0 when a `hit` is recorded.
+- a `foul` increases strikes up to 2. With no strikes, a foul makes it 1 strike. 
+ With 1 strike, a foul makes it 2 strikes.
+ With two strikes a foul has no effect, count stays at 2 strikes.
+  
+  */
+  function handleStrike() {
+    setStrikes(strikes+1);
+
+  }
+  function handleHit() {
+    setStrikes(0);
+    setBalls(0);
+  }
+  function handleFoul() {
+    if(strikes < 2){
+        setStrikes(strikes+1);
+    }
+  }
+  function handleBall() {
+    setBalls(balls+1);
+  }
+
+  useEffect(()=>{
+    if (strikes === 3 || balls === 4){
+        setStrikes(0);
+        setBalls(0);
+    }
+  }, [strikes, balls])
+
+
   return (
     <>
-      <button>Strike</button>
-      <button>Ball</button>
-      <button>Foul</button>
-      <button>Hit</button>
+    {console.log('Strikes, Balls', strikes, balls)}
+      <button onClick={handleStrike}>Strike</button>
+      <button onClick={handleBall}>Ball</button>
+      <button onClick={handleFoul}>Foul</button>
+      <button onClick={handleHit}>Hit</button>
     </>
   );
 }
--- src/Dashboard.test.js
+++ src/Dashboard.test.js
@@ -1,11 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import React from 'react';
 import Dashboard from './Dashboard';
-// import '@testing-library/react/cleanup-after-each';
+import '@testing-library/react/cleanup-after-each';
 
 describe('<Dashboard />', () => {
-    it('should show a button with text Strike', () => {
+    it('should show buttons with text Strike, Ball, Foul, Hit', () => {
         const dash = render(<Dashboard />)
         dash.getByText(/Strike/)
+        dash.getByText(/Ball/)
+        dash.getByText(/Foul/)
+        dash.getByText(/Hit/)
+    })
+    it('Buttons should fire events when clicked', () => {
+        const dash = render(<Dashboard />)
+        fireEvent.click(dash.getByText(/Strike/));
+        fireEvent.click(dash.getByText(/Ball/));
+        fireEvent.click(dash.getByText(/Foul/));
+        fireEvent.click(dash.getByText(/Hit/));
     })
 })
